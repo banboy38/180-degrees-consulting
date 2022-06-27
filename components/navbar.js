c97f5f597ccc02659d46e191e2ea8260a33d2ab2
Rahul Raj Soren <rahulsoren90@gmail.com>
2022-06-28 02:55:07 +0530

    Navbar to be vertical

--- components/navbar.js
+++ components/navbar.js
@@ -1,78 +1,100 @@
+import {
+  Box,
+  Flex,
+  Img,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+import {logoIcon} from './logo.png'
+import ThemeToggleButton from './theme-toggle-button';
+const Links = ['Dashboard', 'Projects', 'Team'];
 
-import NextLink from 'next/link'
-import {Container, Box, Link, Stack, Heading, Flex, Menu, MenuItem, MenuList, MenuButton, IconButton, useColorModeValue} from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
-import ThemeToggleButton from './theme-toggle-button'
-const LinkItem = ({ href, path, children}) =>{
-    const active = path === href
-    const inactiveColor = useColorModeValue('gray2000, whiteAlpha.900')
-    return(
-        <NextLink href={href}>
-            <Link p={2} bg={active ? 'glassTeal' : undefined} color={active ? '#202023' : inactiveColor}>
-                {children}
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
 
-            </Link>
-        </NextLink>
-    )
+export default function WithAction() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+    
+  return (
+    <>
+      <Box className='z-20 top-0 right-0' bg={useColorModeValue('gray.100', 'gray.900')} px={5} pt={2} w='100%' position="fixed">
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box><Img src="./logo.png" alt="icon" pt={5}/></Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+            
+          </HStack>
+          <Flex alignItems={'center'} gap={3}>
+            <ThemeToggleButton/>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'md'}
+                variant={'link'}
+                cursor={'pointer'}
+                minW={0}>
+                <IconButton
+                  size={'sm'}
+                  icon={<HamburgerIcon/>}
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
 }
-const Navbar = props => {
-    const {path} = props
-
-    return(
-        <Box position="fixed"
-             as="nav"
-             w="100%"
-             bg={useColorModeValue('#ffffff40','#20202380')}
-             style={{backdropFilter:'blur(10px)'}}
-             zIndex={1}
-             {...props}>
-                <Container display="flex" p={2} maxW="100%" wrap="wrap" align="center" justify="space-between">
-                    <Flex align="center">
-                        <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-                                180DC - BITM
-                        </Heading>
-                    </Flex>
-
-                    <Stack 
-                        direction={{base:'column',md:'row'}} 
-                        display={{base:'none', md:'flex'}}
-                        width={{base:'full', md:'auto'}} 
-                        alignItems="center" 
-                        flexGrow={1} 
-                        mt={{base:4, nmd:0}}>
-                            <LinkItem href="/works" path={path}>
-                                Projects
-                            </LinkItem>
-                            <LinkItem href="/contact" path={path}>
-                                Contact
-                            </LinkItem>
-                    </Stack>
-
-                    <Box flex={1} align="right">
-                        <ThemeToggleButton/>
-                        <Box ml={2} display={{base: 'inline-block', md:'none'}}>
-                            <Menu>
-                                <MenuButton as={IconButton} icon={<HamburgerIcon/>} variant="outline" aria-label="Options"/>
-                                <MenuList>
-                                    <NextLink href="/" passHref>
-                                        <MenuItem as={Link}>About</MenuItem>
-                                    </NextLink>
-                                    <NextLink href="/works" passHref>
-                                        <MenuItem as={Link}>Works</MenuItem>
-                                    </NextLink>
-                                    <NextLink href="/contact" passHref>
-                                        <MenuItem as={Link}>Contact</MenuItem>
-                                    </NextLink>
-                                    <NextLink href="https://github.com/Roninraj/nextjs-portfolio-3d" passHref>
-                                        <MenuItem as={Link}>View Source</MenuItem>
-                                    </NextLink>
-                                </MenuList>
-                            </Menu>
-                        </Box>
-                    </Box>
-                </Container>
-             </Box>
-    )
-}
-
-export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Container, Text, useColorMode, Button,IconButton} from '@chakra-ui/react'
+import { Container, Text, useColorMode, Button,IconButton, Box} from '@chakra-ui/react'
 import ThemeToggleButton from '../components/theme-toggle-button'
 import Navbar from '../components/navbar'
 const Page = () => {
   return (
     <>
-      <Container>
-        <Navbar/>
-      </Container>
+    <Box><Navbar/></Box>      
     </>
   )
 }
